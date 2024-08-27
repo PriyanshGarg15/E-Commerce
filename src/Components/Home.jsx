f0605cc3e3bfdbb364f03a5f81085da5b606545e
priyanshgarg15 <priyanshgargagra123@gmail.com>
2024-08-27 23:21:35 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Layout from "./Layout"
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation ,Pagination } from 'swiper/modules';
@@ -7,6 +8,80 @@ import 'swiper/css/pagination';
 
 
 const Home = () =>{
+    const [products,setProducts]=useState([
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/a.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/b.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/c.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/d.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/e.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/f.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/g.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/i.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/j.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/k.jpg'
+        },
+        {
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/l.jpg'
+        },{
+            title:'New blue shirts mens',
+            price: 1200,
+            discount:15,
+            thumbnail:'/products/l.jpg'
+        }
+
+    ]);
     return(
         <Layout>
              <div>
@@ -26,9 +101,27 @@ const Home = () =>{
                     <h1 className="text-4xl font-bold text-center">
                         Latest Products</h1>
                     <p className="mx-auto text-center 
-                    text-gray-600 w-7/12 mb-6">
+                    text-gray-600 w-7/12 mb-16">
                         Discover exceptional quality and innovation in every item. Crafted with precision, each product embodies excellence and reliability, ensuring satisfaction in every use. Whether you're seeking top-tier performance or elegant design, our collection meets all your needs, offering the perfect blend of style and functionality. Explore now and elevate your experience with products designed to exceed expectations.</p>
-                    <div className="w-10/12 mx-auto"></div>
+                    <div className="w-10/12 mx-auto grid grid-cols-4 gap-12">
+                        {
+                            products.map((item,index)=>(
+                                <div key={index} className="bg-white shadow-lg">
+                                    <img src={item.thumbnail} alt="" />
+                                    <div className="p-4">
+                                        <h1 className="text-lg font-semibold">{item.title}</h1>
+                                        <div className="space-x-2">
+                                            <label className="font-bold text-lg" htmlFor="">₹{item.price-(item.price*item.discount)/100}</label>
+                                            <del>₹{item.price}</del>
+                                            <label className="text-gray-600" htmlFor="">({item.discount}%)</label>
+                                        </div>
+                                        <button className="bg-green-500 py-2 w-full rounded text-white font-semibold mt-4">Buy Now</button>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    
+                    </div>
                 </div>
             </div>
         </Layout>
